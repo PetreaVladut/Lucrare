42ff8bcafab234ed47978e58c28272cabbdd0bf0
PetreaVladut <93314910+PetreaVladut@users.noreply.github.com>
2022-06-08 17:43:52 +0300

    Update gen_file.h

--- gen_file.h
+++ gen_file.h
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <string.h>
 #include <math.h>
+#include <vector>
 using namespace std;
 
-char* generare(int p){
+char* generare(int p, int c){
     char *s;
     s=(char*)calloc(25,sizeof(char));
     strcpy(s,"fisier1\0");
@@ -12,7 +13,14 @@ char* generare(int p){
         strcat(s,"0\0");
         strcat(s,".txt\0");
         ofstream ceva(s);
-    for(long int i=0;i<pow(10,p);i++)
-        ceva<<rand()%1000<<'\n';
+                    vector<int> a;
+            for(long int i=0;i<pow(10,p);i++)
+                a.push_back(rand()%1000);
+        if(c==2)
+            sort(a.begin(),a.end());
+        if(c==3)
+            sort(a.begin(),a.end(),greater<int>());
+        for(long int i=0;i<pow(10,p);i++)
+        ceva<<a[i]<<'\n';
         return s;
 }
